6f375d084a3c1f8641ac53107e42dc02c8d81393
Neggia <test@abc..com>
2023-10-24 00:40:54 +0200

    Added entities

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,3 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-@Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-})
-export class DatabaseModule {}
-
 // import { Module } from '@nestjs/common';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 // import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -31,7 +7,11 @@ export class DatabaseModule {}
 //     TypeOrmModule.forRootAsync({
 //       imports: [ConfigModule],
 //       useFactory: (configService: ConfigService) => ({
-//         type: 'sqlite',
+//         type: 'mysql',
+//         host: configService.get('DB_HOST'),
+//         port: +configService.get('DB_PORT'),
+//         username: configService.get('DB_USERNAME'),
+//         password: configService.get('DB_PASSWORD'),
 //         database: configService.get('DB_NAME'),
 //         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 //         synchronize: true,
@@ -41,3 +21,23 @@ export class DatabaseModule {}
 //   ],
 // })
 // export class DatabaseModule {}
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
