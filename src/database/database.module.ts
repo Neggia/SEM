ac81f961bd3d242599b66c696d7929a17440828c
Neggia <test@abc..com>
2023-10-24 00:20:50 +0200

    Added database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'sqlite',
+//         database: configService.get('DB_NAME'),
+//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+// })
+// export class DatabaseModule {}
