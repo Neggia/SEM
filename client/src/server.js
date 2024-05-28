ad51fd994de06debe2c89ff8feb86d933d923a36
php-wizard <lianobat@hotmail.it>
2024-05-29 01:03:25 +0200

    SSL

--- client/src/server.js
+++ client/src/server.js
@@ -8,24 +8,57 @@ const PORT = process.env.PORT || 8443;
 const keyFile = path.join(__dirname, '/../../data/privkey.pem');
 const certFile = path.join(__dirname, '/../../data/fullchain.pem');
 
-console.log('keyfile:' + keyFile);
-console.log('certFile:' + certFile);
+async function checkFileAndResolveSymlink(filePath) {
+  try {
+    const stats = await fs.lstat(filePath);
+    if (stats.isSymbolicLink()) {
+      const realPath = await fs.realpath(filePath);
+      return { exists: true, isSymlink: true, realPath: realPath };
+    } else {
+      return { exists: true, isSymlink: false, realPath: filePath };
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return { exists: false, isSymlink: false, realPath: null };
+    } else {
+      throw err;
+    }
+  }
+}
 
-// SSL Options
-const httpsOptions = {
-  key: fs.readFileSync(keyFile),
-  cert: fs.readFileSync(certFile),
-};
+(async () => {
+  try {
+    const keyResult = await checkFileAndResolveSymlink(keyFile);
+    const certResult = await checkFileAndResolveSymlink(certFile);
+    if (!keyResult.exists) {
+      throw 'keyFile does not exist';
+    }
+    if (!certResult.exists) {
+      throw 'certFile does not exist';
+    }
 
-// Serv static files of the React build
-app.use(express.static(path.join(__dirname, 'build')));
+    console.log('keyfile:' + keyResult.realPath);
+    console.log('certFile:' + certResult.realPath);
 
-// Handle all other requests with index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+    // SSL Options
+    const httpsOptions = {
+      key: fs.readFileSync(keyResult.realPath),
+      cert: fs.readFileSync(certResult.realPath),
+    };
 
-// Create the HTTPS server
-https.createServer(httpsOptions, app).listen(PORT, () => {
-  console.log(`Server is running on https://localhost:${PORT}`);
-});
+    // Serv static files of the React build
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    // Handle all other requests with index.html
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+
+    // Create the HTTPS server
+    https.createServer(httpsOptions, app).listen(PORT, () => {
+      console.log(`Server is running on https://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error:', err);
+  }
+})();
